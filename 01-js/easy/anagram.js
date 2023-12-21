c4533cd857b4bb41460d76df22b7d9f3204d9870
KRISH708048 <122149980+KRISH708048@users.noreply.github.com>
2023-12-21 23:44:38 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,26 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let j;
+  str1 = str1.toUpperCase();str2 = str2.toUpperCase();
+  const arr1 = str1.split('');
+  const arr2 = str2.split('');
+  //console.log(arr1);
+  for (let i = 0; i < arr1.length; i++) {
+    const char = arr1[i];
+    const ind = arr2.indexOf(char);
+    //console.log(arr2);
+    if (ind !== -1) {
+      arr2.splice(ind, 1);
+    } else {
+      return false;
+    }
+  }
 
+  return true;
 }
 
 module.exports = isAnagram;
