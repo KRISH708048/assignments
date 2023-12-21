a4a220ccbe1dbb2acf392f77f8e9eec9d9e7eff8
KRISH708048 <122149980+KRISH708048@users.noreply.github.com>
2023-12-21 23:47:10 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,19 @@
 */
 
 function isPalindrome(str) {
+
+  if(str.length == 0){
+      return true;
+  }
+  let arr = ((str.toUpperCase()).match(/[A-Z]/g))|| [];
+  for(let i=0;i<=arr.length/2;i++) {
+    if(arr[i]!=arr[arr.length-1-i]){
+      return false;
+    }
+  }
   return true;
 }
+
+
 
 module.exports = isPalindrome;
