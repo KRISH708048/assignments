c5ea77495c3da96b63dee26b4acde4e196584de3
KRISH708048 <122149980+KRISH708048@users.noreply.github.com>
2023-12-21 23:45:56 +0530

    Update calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,52 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+
+    const cleanedExpression = expression.replace(/\s+/g, ' ').trim();
+    if (!/^[\d\s\+\-\*\/\(\)\.]+$/.test(cleanedExpression)) {
+      throw new Error("Invalid expression. It contains non-numerical characters.");
+    }
+
+    try {
+
+      this.result = eval(cleanedExpression);
+    } catch (error) {
+      throw new Error("Invalid expression. Check the arithmetic operations and parentheses.");
+    }
+  }
+}
 
 module.exports = Calculator;
